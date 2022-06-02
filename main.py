1013328e58d28cde6207e61f8665496c1e4864b5
Ruslan <6r0d5k1y@gmail.com>
2022-06-02 21:28:47 +0300

    Docs

--- main.py
+++ main.py
@@ -4,21 +4,39 @@ import json
 import conf
 import Faker_test
 
+
 MODEL = json.loads(conf.text_json1)
-def repeat(number=2):
+
+
+def repeat(number=1000):
+    """
+
+    :param number:
+    :return:
+    """
     def actual_decorator(function):
         def wrapper(*args, **kwargs):
             result = None
-            for _ in range(number):
+            for iter in range(number):
                 result = function(*args, **kwargs)
             return result
 
         return wrapper
 
     return actual_decorator
-@repeat(20)
+
+
+@repeat()
 def main():
+    """
+
+    :return:
+    """
     def modul_title():
+        """
+
+        :return:
+        """
         text = open('BOOK.txt', 'r', encoding='utf8')
         s = text.readline()
         book = s.split(',')
@@ -26,16 +44,19 @@ def main():
         book_2 = random.choice(book)
         MODEL['fields']['title'] = book_ + ',' + book_2
 
-
     modul_title()
 
-
     def modul_pk(start=1, step=1):
+        """
+
+        :param start:
+        :param step:
+        :return:
+        """
         counter = start
         while True:
             yield counter
             counter += step
-
 
     ranger = modul_pk(1, 1)
     for _ in range(10):
@@ -43,40 +64,51 @@ def main():
 
     modul_pk()
 
-
     def model_year():
+        """
+
+        :return:
+        """
         MODEL['fields']['year'] = random.randint(1990, 2020)
         return
 
-
     model_year()
 
-
     def model_pages():
+        """
+    
+        :return: 
+        """
         MODEL['fields']['pages'] = random.randint(1, 1000)
         return
 
-
     model_pages()
 
-
     def model_rating():
+        """
+    
+        :return:
+        """
         MODEL['fields']['rating'] = float(random.randint(-5, 5))
         return
 
-
     model_rating()
 
-
     def modul_isbn13():
+        """
+    
+        :return:
+        """
         MODEL['fields']['isbn13'] = Faker_test.index_book
         return
 
-
     modul_isbn13()
 
-
     def modul_title():
+        """
+
+        :return:
+        """
         text = open('BOOK.txt', 'r', encoding='utf8')
         s = text.readline()
         book = s.split(',')
@@ -84,16 +116,19 @@ def main():
         book_2 = random.choice(book)
         MODEL['fields']['title'] = book_ + ',' + book_2
 
-
     modul_title()
 
-
     def modul_pk(start=1, step=1):
+        """
+
+        :param start:
+        :param step:
+        :return:
+        """
         counter = start
         while True:
             yield counter
             counter += step
-
 
     ranger = modul_pk(1, 1)
     for _ in range(10):
@@ -101,69 +136,85 @@ def main():
 
     modul_pk()
 
-
     def model_year():
+        """
+
+        :return:
+        """
         MODEL['fields']['year'] = random.randint(1990, 2020)
         return
 
-
     model_year()
 
-
     def model_pages():
+        """
+    
+        :return:
+        """
         MODEL['fields']['pages'] = random.randint(1, 1000)
         return
 
-
     model_pages()
 
-
     def model_rating():
+        """
+
+        :return:
+        """
         MODEL['fields']['rating'] = float(random.randint(-5, 5))
         return
 
-
     model_rating()
 
-
     def modul_isbn13():
+        """
+
+        :return:
+        """
         MODEL['fields']['isbn13'] = Faker_test.index_book
         return
 
-
     modul_isbn13()
-
 
     def model_price():
+        """
+
+        :return:
+        """
         MODEL['fields']['price'] = float(random.randint(1, 1000))
         return
 
-
     model_price()
 
-
     def author():
+        """
+
+        :return:
+        """
         MODEL['fields']['author'][1] = Faker_test.name
         MODEL['fields']['author'][0] = Faker_test.name_2
         return
-
 
     author()
 
-
     def model_price():
+        """
+
+        :return:
+        """
         MODEL['fields']['price'] = float(random.randint(1, 1000))
         return
 
-
     model_price()
 
-
     def author():
+        """
+
+        :return:
+        """
         MODEL['fields']['author'][1] = Faker_test.name
         MODEL['fields']['author'][0] = Faker_test.name_2
         return
-
 
     author()
 
@@ -172,27 +223,5 @@ def main():
         print(json.dumps(MODEL, indent=2, ensure_ascii=False))
 
 
-# def repeat(number=2):
-#     """Повтор декорированной функции заданное количество раз.
-#     В результате возвращается последнее значение оригинальной функции.
-#     : number — количество повторов, по умолчанию равно 3.
-#     """
-#
-#     def actual_decorator(function):
-#         def wrapper(*args, **kwargs):
-#             result = None
-#             for _ in range(number):
-#                 result = function(*args, **kwargs)
-#             return result
-#
-#         return wrapper
-#
-#     return actual_decorator
-#print(json.dumps(MODEL, indent=2, ensure_ascii=False))
-#print(type(MODEL))
-# @repeat(20)
-
-
 if __name__ == "__main__":
-
     main()
